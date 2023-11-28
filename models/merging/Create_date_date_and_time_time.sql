3fe9a3b5d174cbbecc022657a69dd5bd37ad490b
waf waf <wafwafglaube@gmail.com>
2023-11-28 14:56:18 +0000

    create a calendar for times_series table

--- models/merging/Create_date_date_and_time_time.sql
+++ models/merging/Create_date_date_and_time_time.sql
@@ -1,4 +1,5 @@
 SELECT *,
 DATE(EXTRACT(YEAR FROM order_purchase_timestamp), EXTRACT(MONTH FROM order_purchase_timestamp), EXTRACT(DAY FROM order_purchase_timestamp)) AS date_date,
 EXTRACT(HOUR FROM order_purchase_timestamp) as time_time,
+
 FROM {{ ref('stg_Olist_big_query__orders') }}
--- models/merging/Times_series1_2.sql
+++ models/merging/Times_series1_2.sql
@@ -1,4 +1,16 @@
-with int as 
+with calendar as (
+    SELECT
+    *
+FROM
+    UNNEST(
+        GENERATE_DATE_ARRAY(
+            '2016-09-04',
+            '2018-09-03',
+            INTERVAL 1 DAY)
+    ) AS date_1) ,
+
+
+int as 
 (
   select 
   date_date,
@@ -15,15 +27,16 @@ group by date_date
 )
 
 SELECT
-  date_date,
+  f.date_1,
   ROUND(turnover,2)as turnover,
   total_orders,
   total_items,
   ROUND((turnover / total_orders),2) AS avg_basket,
-  ROUND((turnover / total_items),2) AS avg_ticket,
+  ROUND((turnover / total_items),2) AS avg_unit_price,
   ROUND((((turnover - LAG(turnover) OVER(ORDER BY date_date ASC)) / LAG(turnover) OVER(ORDER BY date_date ASC))*100),2) AS turnover_growth_percent,
   ROUND((((total_orders - LAG(total_orders) OVER (ORDER BY date_date ASC)) / LAG(total_orders) OVER (ORDER BY date_date ASC)) * 100), 2) AS orders_growth_percent
-
 FROM
-  int
-ORDER BY date_date
+  int as d
+FULL OUTER JOIN calendar as f
+on d.date_date = f.date_1
+ORDER BY f.date_1
