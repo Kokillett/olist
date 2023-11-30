66935e73479c130cb5ab8fdd9d327ac40a782172
Olist Olist <olist.bootcamp@gmail.com>
2023-11-30 14:39:14 +0000

    Commit added review_score to three models about sellers

--- models/models/Top_10_sellers_qty_items.sql
+++ models/models/Top_10_sellers_qty_items.sql
@@ -2,7 +2,8 @@ SELECT
     seller_id
     ,product_category_name_english
     ,COUNT(product_id) as number_of_products
+    ,review_score
 FROM {{ ref('sellers_table') }}
-GROUP BY seller_id, product_category_name_english
+GROUP BY seller_id, product_category_name_english, review_score
 ORDER BY COUNT(product_id) DESC
 LIMIT 10
