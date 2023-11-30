2d123840b4ac5745f2070e807f912573fed908c8
Olist Olist <olist.bootcamp@gmail.com>
2023-11-30 13:14:24 +0000

    commit

--- models/intermediate/sellers_qty.sql
+++ models/intermediate/sellers_qty.sql
@@ -1,5 +1,7 @@
 SELECT
     seller_id
     ,COUNT(order_id) as number_of_orders
+    ,COUNT(product_id) as number_of_products
+    ,SUM(payment_value) as seller_total_value
 FROM {{ ref('best_sellers_int') }}
 GROUP BY seller_id
