4ae4d3697991a91759f6f49bdb85e5aa584a0718
Olist Olist <olist.bootcamp@gmail.com>
2023-11-30 10:40:04 +0000

    commit

---
+++ models/intermediate/best_sellers_int.sql
@@ -0,0 +1,12 @@
+{{ config(materialized='best_sellers') }}
+
+SELECT
+    date_1
+    ,order_id
+    ,customer_state
+    ,product_category_name_english
+    ,product_id
+    ,payment_value
+    ,seller_id
+    ,review_score
+FROM {{ ref('Times_series1_2') }}
