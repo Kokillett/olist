db9e228ef6b5db0af571b071e34c52a616a31072
Olist Olist <olist.bootcamp@gmail.com>
2023-12-01 14:50:49 +0000

    Commit deleting reccuring tables blabla

--- models/models/recuring_order_by_sellers_id.sql
+++
@@ -1,9 +0,0 @@
-select 
-customer_id
-,seller_id
-,count(order_id) AS count_order
-from {{ ref('int_customer_turnover') }}
-GROUP BY 
-customer_id
-,seller_id
-ORDER BY count_order DESC
--- models/recuring_order_by_product_category_name.sql
+++
@@ -1,9 +0,0 @@
-select 
-customer_id
-,product_category_name_english
-,count(order_id) AS count_order
-from {{ ref('int_customer_turnover') }}
-GROUP BY 
-customer_id
-,product_category_name_english
-ORDER BY count_order DESC
