44b2c3755a28fd1a22507cbd52810f76c5e46e9c
oscar muls <oscarmuls@hotmail.com>
2023-12-04 13:30:44 +0000

    added the occurence count per order_id

--- models/staging/Olist_big_query/stg_Olist_big_query__order_items.sql
+++ models/staging/Olist_big_query/stg_Olist_big_query__order_items.sql
@@ -1,24 +1,32 @@
-with 
-
-source as (
-
-    select * from {{ source('Olist_big_query', 'order_items') }}
-
+WITH source AS (
+    SELECT * FROM {{ source('Olist_big_query', 'order_items') }}
 ),
-
-renamed as (
-
-    select
+renamed AS (
+    SELECT
         order_id,
-        order_item_id,
         product_id,
         seller_id,
         shipping_limit_date,
         price,
         freight_value
-
-    from source
-
+    FROM source
 )
+SELECT
+    order_id,
+    product_id,
+    seller_id,
+    shipping_limit_date,
+    price,
+    freight_value,
+    COUNT(*) AS occurrence_count
+/*freight_value is correct, price however needs to be multiplied by the occurence_count */
+FROM renamed
+GROUP BY
+    order_id,
+    product_id,
+    seller_id,
+    shipping_limit_date,
+    price,
+    freight_value
 
-select * from renamed
+order by order_id
