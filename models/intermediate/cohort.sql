039d980b999bea55688cb4f63f6ec9d040bcb62d
Olist Olist <olist.bootcamp@gmail.com>
2023-12-05 13:01:12 +0000

    Deleting useless table

--- models/intermediate/cohort.sql
+++
@@ -1,6 +0,0 @@
-SELECT
-    customer_id
-    ,MIN(date_1) AS first_purchase_date
-    ,MIN(CAST(CONCAT(EXTRACT(year FROM date_1),"-", EXTRACT(month FROM date_1),"-", 1) AS DATE)) AS cohort_date
-FROM {{ ref('CentralizedData') }}
-GROUP BY customer_id
