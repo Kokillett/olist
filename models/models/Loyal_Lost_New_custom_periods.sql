9886bd16ea816b92866d3adbf5f9a033f67dc433
waf waf <wafwafglaube@gmail.com>
2023-12-05 15:16:07 +0000

    recommit

--- models/models/Loyal_Lost_New_custom_periods.sql
+++ models/models/Loyal_Lost_New_custom_periods.sql
@@ -1,17 +1,17 @@
 WITH CustomerMonthsCTE AS (
   SELECT
-    customer_unique_id,
+    customer_id,
     MAX(CASE WHEN DATE(date_1) BETWEEN '2018-03-01' AND '2018-05-31' THEN 1 ELSE 0 END) AS period_1,
     MAX(CASE WHEN DATE(date_1) BETWEEN '2018-06-01' AND '2018-08-31' THEN 1 ELSE 0 END) AS period_2
-  FROM {{ ref('join_date_1_unique_id') }}
+  FROM {{ ref('CentralizedData') }}
   WHERE DATE(date_1) BETWEEN '2018-03-01' AND '2018-08-31'
-  GROUP BY customer_unique_id
+  GROUP BY customer_id
 )
 
 SELECT
-  COUNT(DISTINCT CASE WHEN period_1 = 1 AND period_2 = 1 THEN customer_unique_id END) AS loyal_count,
-  COUNT(DISTINCT CASE WHEN period_1 = 1 AND period_2 = 0 THEN customer_unique_id END) AS lost_count,
-  COUNT(DISTINCT CASE WHEN period_1 = 0 AND period_2 = 1 THEN customer_unique_id END) AS new_count,
-  COUNT(DISTINCT CASE WHEN period_1 = 1 OR period_2 = 1 THEN customer_unique_id END) AS total_at_beginning,
-  COUNT(DISTINCT CASE WHEN period_1 = 0 AND period_2 = 1 THEN customer_unique_id END) / COUNT(DISTINCT CASE WHEN period_1 = 1 OR period_2 = 1 THEN customer_unique_id END) * 100 AS churn_rate
+  COUNT(DISTINCT CASE WHEN period_1 = 1 AND period_2 = 1 THEN customer_id END) AS loyal_count,
+  COUNT(DISTINCT CASE WHEN period_1 = 1 AND period_2 = 0 THEN customer_id END) AS lost_count,
+  COUNT(DISTINCT CASE WHEN period_1 = 0 AND period_2 = 1 THEN customer_id END) AS new_count,
+  COUNT(DISTINCT CASE WHEN period_1 = 1 OR period_2 = 1 THEN customer_id END) AS total_at_beginning,
+  COUNT(DISTINCT CASE WHEN period_1 = 0 AND period_2 = 1 THEN customer_id END) / COUNT(DISTINCT CASE WHEN period_1 = 1 OR period_2 = 1 THEN customer_id END) * 100 AS churn_rate
 FROM CustomerMonthsCTE
