1c92f1e69ff8fffb0aa4e2e27cd0f2ca284dc139
waf waf <wafwafglaube@gmail.com>
2023-12-07 15:22:30 +0000

    replace turnover by  items_price to be more consistent since we don't use freight value

--- models/intermediate/total_order_and_turnover.sql
+++ models/intermediate/total_order_and_turnover.sql
@@ -1,6 +1,6 @@
 SELECT 
 sum(total_number_orders) as total_number_orders
-,sum(turnover) as turnover
+,sum(item_price) as item_price
 ,date_1, time_time
 from {{ ref('CentralizedData') }}
 GROUP BY date_1,time_time
